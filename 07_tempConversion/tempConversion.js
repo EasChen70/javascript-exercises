cef903cbaa1a570e0f06dc5bf3ffa1e1b4749eb5
EasChen70 <eason_chen@uri.edu>
2024-02-03 19:42:20 -0500

    Finished problem set

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  if(num == 32){
+    return 0;
+  }
+  let result = (5/9);
+  result *= (num - 32);
+  return parseFloat(result.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  if(num == 0){
+    return 32;
+  }
+  let result = (9/5) * num;
+  result += 32;
+  return parseFloat(result.toFixed(1))
 };
 
 // Do not edit below this line
